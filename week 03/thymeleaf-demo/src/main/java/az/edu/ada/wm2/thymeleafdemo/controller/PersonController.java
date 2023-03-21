b68353cb31b4e308382ec477e02200db75a8ebb9
Nargiz Heybatova <nargizheybatova07@gmail.com>
2023-03-21 06:48:26 +0400

    Changes for week 3/4

--- week 03/thymeleaf-demo/src/main/java/az/edu/ada/wm2/thymeleafdemo/controller/PersonController.java
+++ week 03/thymeleaf-demo/src/main/java/az/edu/ada/wm2/thymeleafdemo/controller/PersonController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -36,5 +37,14 @@ public class PersonController {
         return "redirect:/";
     }
 
+    @GetMapping("/update/{id}")
+    public ModelAndView showUpdatePage(@PathVariable String id) {
+        Person found = personService.getById(id);
+        ModelAndView mv = new ModelAndView( "update_person");
+        mv.addObject("person", found);
+
+        return mv;
+
+    }
 }
 
